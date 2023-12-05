b7691e9fb1e31dc3662bb6e343322e4bd7c62ec7
SR-Hossain <sshr.rezvi@gmail.com>
2023-12-05 13:36:54 +0600

    fixed text size in todo tiles

--- lib/presentation/widget/todo_tile_widget.dart
+++ lib/presentation/widget/todo_tile_widget.dart
@@ -11,34 +11,52 @@ class TodoTileWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Flexible(
+        Expanded(
+            flex: 0,
             child: Text(
-          title,
-          style: const TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-        )),
+              '$title\n',
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 2,
+              // overflow: TextOverflow.ellipsis,
+              // textAlign: TextAlign.center,
+            )),
         //
 
         const Gap(10),
 
-        Flexible(
+        Expanded(
+          // flex: 3,
+          // fit: FlexFit.loose,
           child: Text(
             description,
             overflow: TextOverflow.ellipsis,
             style: const TextStyle(
               fontSize: 13.0,
-              fontFamily: 'Roboto',
               color: Color(0xFF212121),
             ),
-            maxLines: 8,
+            maxLines: 6,
             textAlign: TextAlign.left,
           ),
         ),
+        // SizedBox(
+        //   width: 150,
+        //   // fit: FlexFit.loose,
+        //   child: Text(
+        //     description,
+        //     overflow: TextOverflow.ellipsis,
+        //     style: const TextStyle(
+        //       fontSize: 13.0,
+        //       color: Color(0xFF212121),
+        //     ),
+        //     maxLines: 5,
+        //     textAlign: TextAlign.left,
+        //   ),
+        // ),
+        // Text('bolod sami'),
+        // Text('bolod sami'),
       ],
     );
   }
